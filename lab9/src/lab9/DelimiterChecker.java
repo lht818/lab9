a2376a89eafe56e85534089f3c8341672e22950b
lht818 <lht818@L2PC7.ad.utulsa.edu>
2017-10-24 15:52:58 -0500

    this is the final lab. i have made some corrections.

--- lab9/src/lab9/DelimiterChecker.java
+++ lab9/src/lab9/DelimiterChecker.java
@@ -3,8 +3,8 @@ package lab9;
 import java.util.Stack;
 import java.util.Scanner;
 
-	    public class DelimiterChecker {
-		private char[] stackArray;
+	public class DelimiterChecker {
+		
 		/**
 		 * 
 		 * @param a is the opening delimiter
@@ -29,39 +29,57 @@ import java.util.Scanner;
 		 * @return
 		 */
 		
-		public boolean check(String str)
+		public boolean Check(String str)
 		{   
 			boolean valid = false;
 			int size = str.length();
+			Stack test = new Stack();
 			
-			// push the opening delimiter into the stack
-			for( int i = 0; i< size; i++)
-			{
-				if(str.charAt(i)== '(' || str.charAt(i) == '{' || str.charAt(i) =='[')
-					 DelimiterChecker.push(str.charAt(i));
-			}
 			// the stack is empty 
-			if (stackArray== null)
+			if (test== null)
 				System.out.println(" missing left delimiter");
-			else 
+			else
 			{
-				// check the closing delimiter
-				for(int j = 0; j < size; j++)
+				// push the opening delimiter into the stack
+				for( int i = 0; i< size; i++)
 				{
-					if(str.charAt(j) == ')' || str.charAt(j) == '}' || str.charAt(j) ==']')
-					{
-						DelimiterChecker.pop();
-						
-				
-					}
-					
+					if(str.charAt(i)== '(' || str.charAt(i) == '{' || str.charAt(i) =='[')
+						 test.push(str.charAt(i));
 				}
+			}
+			// check if the closing delimiter match opening delimiter 
+        	while(test!=null){
+	        	for( int i = 0; i< size; i++)
+				{
+					if(str.charAt(i) == ')' || str.charAt(i) == '}' || str.charAt(i) ==']')
+					{
+						char x = (char) test.pop();
+						if(DelimiterCheck(x,str.charAt(i))== true){
+							System.out.println("the delimiters are match");
+							return valid= true;
+						}
+						else{
+							System.out.println("the delimiters are unmatch");
+							return valid = false;
+						}
+					}
+	        	}
+				return valid;
+			}
+            return valid;
+        }
+        
+		public static void main(String[]args)
+		{
+			Scanner console = new Scanner(System.in);
+			System.out.println("Please enter the statement:");
+			String input = console.next();
 				
-			
+			DelimiterChecker object =new DelimiterChecker();
+			object.Check(input);
 		}
-
 		
-		}
+		
 	}
 
 
